719364ed13158f6751197fe6e8c45bccd9c8f277
dima-in <dima-in@mail.ru>
2023-03-04 23:22:38 +0300

    созданы таблицы orders, order_details, customers

--- DBTable.py
+++ DBTable.py
@@ -6,36 +6,34 @@ def create_table():
         'host': '127.0.0.1',
         'user': 'oilorder_admin',
         'password': 'oilpasswd',
-        'database': 'OilOrders'
+        'database': 'oilordersDB'
     }
     with UseDatabase(config) as cursor:
         # Создание таблицы orders (если она не существует)
         cursor.execute("""CREATE TABLE IF NOT EXISTS orders (
-                          id INTEGER PRIMARY KEY AUTOINCREMENT,
-                          customer_id INTEGER NOT NULL,
+                          id INT PRIMARY KEY AUTO_INCREMENT,
                           data DATE NOT NULL,
                           shipping_date DATE NOT NULL,
-                          total_price REAL NOT NULL,
-                          FOREIGN KEY (customer_id) REFERENCES customer(id)
+                          total_price FLOAT NOT NULL
                           )""")
 
         # Создание таблицы order_details (если она не существует)
         cursor.execute("""CREATE TABLE IF NOT EXISTS order_details (
-                          id INTEGER PRIMARY KEY AUTOINCREMENT,
-                          order_id INTEGER NOT NULL,
-                          oil_name TEXT NOT NULL,
-                          volume INTEGER NOT NULL,
-                          price REAL NOT NULL,
+                          id INT PRIMARY KEY AUTO_INCREMENT,
+                          order_id INT NOT NULL,
+                          oil_name VARCHAR(100) NOT NULL,
+                          volume INT NOT NULL,
+                          price FLOAT NOT NULL,
                           FOREIGN KEY (order_id) REFERENCES orders(id)
                           )""")
 
         # Создание таблицы customers (если она не существует)
         cursor.execute("""CREATE TABLE IF NOT EXISTS customers (
-                          id INTEGER PRIMARY KEY AUTOINCREMENT,
-                          order_id INTEGER NOT NULL,
-                          name TEXT NOT NULL,
-                          sur_name TEXT NOT NULL,
-                          phone TEXT NOT NULL,
+                          id INT PRIMARY KEY AUTO_INCREMENT,
+                          order_id INT NOT NULL,
+                          name VARCHAR(50) NOT NULL,
+                          sur_name VARCHAR(50) NOT NULL,
+                          phone VARCHAR(20) NOT NULL,
                           FOREIGN KEY (order_id) REFERENCES orders(id)
                           )""")
 
